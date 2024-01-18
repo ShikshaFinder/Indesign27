98471d2077e4c68d9875e727a4826e4afba9b85e
ShikshaFinder <janiharsh794@gmail.com>
2024-01-19 02:45:16 +0530

    indesign27

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Indesign27",
+	description: "Let's Create Heaven At Home",
 	navItems: [
 		{
 			label: "Home",
